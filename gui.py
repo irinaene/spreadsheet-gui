223ee1ac22d03554a4c5ad9c24cc21a88e105494
Irina Ene <irina.ene@berkeley.edu>
2025-06-11 13:27:05 -0700

    handle more input file formats

--- gui.py
+++ gui.py
@@ -130,53 +130,87 @@ def readInputData(window):
     
     # get dir name from CLI args
     input_dir = sys.argv[1]
+    # get all relevant files from input_dir
     files = glob.glob(f"{input_dir}/*.csv")
     files.extend(glob.glob(f"{input_dir}/*.CSV"))
-    # add header with column descriptions
-    window.listbox_in.insert(tk.END, "Date - Description - Category - Amount")
-    for input_file in files[:3]:
+    # add header with column descriptions, nicely formatted
+    c1 = "Date".ljust(10)
+    c2 = "Description".ljust(50)
+    c3 = "Category".ljust(20)
+    header_line = f"{c1} | {c2} | {c3} | Amount"
+    window.listbox_in.insert(tk.END, header_line)
+    # define separator line between different files
+    desc_len, cat_len = 50, 20  # max lengths for various entries
+    sep_line = "-" * 10 + "-|-" + "-" * desc_len + "-|-" + "-" * cat_len + "-|-" + "-" * 11
+    window.listbox_in.insert(tk.END, sep_line)
+    for input_file in files:
         print(input_file)
         # import data using csv module
         with open(input_file) as fin:
             csv_reader = csv.reader(fin, delimiter=',', quotechar='"')
-            header = " - ".join(next(csv_reader))
+            header = ",".join(next(csv_reader))
             for row in csv_reader:
+                row_str = " ".join(row).lower()
                 # skip the autopay lines and empty line
-                if ("autopay" in " ".join(row).lower()) or (len(row) == 0):
+                if ("autopay" in row_str) or ("automatic payment" in row_str) or (len(row) == 0):
                     continue
                 # format row depending on csv header info
-                new_row = formatRow(row, header=header)
+                new_row = formatRow(row, header=header, desc_len=desc_len, cat_len=cat_len)
                 # create one long string per row
-                concat_row = " - ".join(new_row)
-                # add to list A
+                concat_row = " | ".join(new_row)
+                # add to input listbox
                 window.listbox_in.insert(tk.END, concat_row)
         # add a line to separate between the different data sources
-        window.listbox_in.insert(tk.END, "-" * 100)
+        window.listbox_in.insert(tk.END, sep_line)
     # fix last line not showing properly
     window.listbox_in.insert(tk.END, "")
 
-def formatRow(row, header):
+def formatRow(row, header, desc_len, cat_len):
     """Function to format a row given the header of the input csv file."""
     
-    # max lengths for various entries
-    desc_len, cat_len = 42, 14
     # use header to decide what info is contained in row
-    if header == "Date - Description - Amount":
+    if header == "Date,Description,Amount":
         # change the date to yyyy-mm-dd format
         date = datetime.strptime(row[0], "%m/%d/%Y").strftime("%Y-%m-%d")
         row[0] = date
         # add a default category
-        row.insert(2, "food")
-    elif header == "Transaction Date - Posted Date - Card No. - Description - Category - Debit - Credit":
-        # print(new_row)
-        idx = [0, 3, 4, 5]
+        row.insert(2, "Food")
+        # amount: ensure consistent signs, debit -, credit +
+        row[3] = f"{-1 * float(row[3]):.2f}"
+    elif header == "Transaction Date,Posted Date,Card No.,Description,Category,Debit,Credit":
+        idx = [0, 3, 4, 5, 6]
         row = [row[id] for id in idx]
+        # amount: ensure consistent signs, debit -, credit +
+        if row[3] == "":
+            row[3] = row[4]
+        else:
+            row[3] = f"{-1 * float(row[3]):.2f}"
+        # remove credit info
+        row.pop(len(row) - 1)
+    elif header == "Details,Posting Date,Description,Amount,Type,Balance,Check or Slip #":
+        idx = [1, 2, 3]
+        row = [row[id] for id in idx]
+        # change the date to yyyy-mm-dd format
+        date = datetime.strptime(row[0], "%m/%d/%Y").strftime("%Y-%m-%d")
+        row[0] = date
+        # add a default category
+        row.insert(2, "Food")
+    elif header == "Transaction Date,Post Date,Description,Category,Type,Amount,Memo":
+        idx = [0, 2, 3, 5]
+        row = [row[id] for id in idx]
+        # change the date to yyyy-mm-dd format
+        date = datetime.strptime(row[0], "%m/%d/%Y").strftime("%Y-%m-%d")
+        row[0] = date
     # set max length for description
     if len(row[1]) < desc_len:
         row[1] = row[1].ljust(desc_len)
+    else:
+        row[1] = row[1][:desc_len]
     # set max length for category
     if len(row[2]) < cat_len:
         row[2] = row[2].ljust(cat_len)
+    else:
+        row[2] = row[2][:cat_len]
     
     return row
 
